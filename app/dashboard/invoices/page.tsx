9eac565a48d8cb6813761f708539d27697f786f5
YanFeng <w954083089@outlook.com>
2025-02-14 15:30:09 +0800

    deploy

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,7 +1,6 @@
-import { fetchRevenue } from "@/app/lib/data";
-import { InvoicesTable } from "@/app/ui/invoices/table";
+
+// import { InvoicesTable } from "@/app/ui/invoices/table";
 export default function Page() {
-  // const invoices = await fetchRevenue();
   return <p>Invoices Page</p>
 
 }
